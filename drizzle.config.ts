1376be0f7f960cc1c2df1e8c50ebc3023217a679
pkeen <pkeen7@gmail.com>
2024-11-13 19:27:23 -0800

    add script for pushing database schema changes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "drizzle-kit";
 
-const databaseUrl =
-	process.env.DATABASE_URL ||
-	"postgres://postgres:password@localhost:5432/postgres";
+const databaseUrl = process.env.DATABASE_URL || "databaseURl not found";
 
 export default defineConfig({
 	dialect: "postgresql", // 'mysql' | 'sqlite' | 'turso'
 	schema: "./src/db/schema.ts",
-	out: "./migrations", // Directory to store migration files
+	// out: "./migrations", // Directory to store migration files
 	dbCredentials: {
-		url: databaseUrl,
+		url:databaseUrl,
 	},
 });
